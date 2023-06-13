864d59d3119595085af0464a1eb18ea88b35d584
Keith Scheldt <keithscheldt@gmail.com>
2023-06-12 20:06:34 -0400

    clean up

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -14,7 +14,7 @@ const CanvasModel  = () => {
       gl={{ preserveDrawingBuffer: true }}
       className="w-full max-w-full h-full transition-all ease-in">
       <ambientLight intensity={0.5} />
-      <Environment files="../src/assets/golf1k.hdr" />
+      {/* // <Environment files="../src/assets/golf1k.hdr" /> */}
       
       <CameraRig>
         <Backdrop />
